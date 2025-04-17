0216a774fcef032c4ab4548ffab0b4e09ba91f31
dodesu <do204contact@gmail.com>
2025-04-17 23:20:44 +0700

    feat: New Fish Backet btn switch to id input box when click

--- public/js/load_animation.js
+++ public/js/load_animation.js
@@ -1,3 +1,5 @@
+const link = document.querySelector('#new-fish');
+
 const loadComplete = () => {
     const loader = document.querySelector('#loader');
     const width = window.innerWidth;
@@ -7,6 +9,44 @@ const loadComplete = () => {
 
     const deriveScale = diagonal / circle.scrollWidth;
     circle.style.transform = `scale(${deriveScale})`;
-    setTimeout(() => { loader.remove() }, 1000);
+    setTimeout(() => { loader.remove() }, 700);
 }
+
+function makeEditable() {
+    const currentSpan = document.querySelector('#span-new-fish');
+    if (!currentSpan) return;
+
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.value = '';
+    input.className = 'bg-gray-800 text-white px-2 py-1 rounded w-full focus:outline-none';
+    input.id = 'input-new-fish';
+
+    const save = () => {
+        const newSpan = document.createElement('span');
+        newSpan.id = 'span-new-fish';
+        newSpan.className = 'px-2 py-1 w-full';
+        newSpan.textContent = "New Fish Basket";
+        link.replaceChild(newSpan, input);
+    };
+
+    input.addEventListener('keydown', function (e) {
+        if (e.key === 'Enter') {
+            save();
+        }
+    });
+
+    input.addEventListener('blur', () => {
+        setTimeout(() => {
+            if (link.contains(input)) {
+                save();
+            }
+        }, 0);
+        //When entering the input, it will call the blur event immediately, so we need to delay it a bit
+    });
+    link.replaceChild(input, currentSpan);
+    input.focus();
+}
+
 loadComplete();
+link.addEventListener('click', makeEditable);
