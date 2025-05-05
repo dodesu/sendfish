269c5e9e29d8725176533f2d6f5be2ee253e050f
dodesu <do204contact@gmail.com>
2025-05-06 03:31:28 +0700

    fix: add missing utils.js after previous merge (#10)

---
+++ public/js/utils/utils.js
@@ -0,0 +1,18 @@
+
+/**
+ * Serialize raw key to base64.
+ * @param {ArrayBuffer} value 
+ * @returns Base64 public key 
+ */
+export const binaryToBase64 = (value) => {
+    return btoa(String.fromCharCode(...new Uint8Array(value)));
+}
+
+/**
+ * Deserialize base64 key to raw key.
+ * @param {string} value
+ * @returns {ArrayBuffer} raw key
+ */
+export const base64ToBinary = (value) => {
+    return Uint8Array.from(atob(value), c => c.charCodeAt(0));
+}
